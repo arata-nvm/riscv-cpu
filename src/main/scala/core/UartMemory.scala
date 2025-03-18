d112c8268a7efa381a9d9cf17a5fdec582e8a32b
Arata <arata04962@gmail.com>
2025-03-18 17:56:41 +0900

    feat: impl UART LSR and RBR

--- src/main/scala/core/UartMemory.scala
+++ src/main/scala/core/UartMemory.scala
@@ -5,7 +5,8 @@ import chisel3.util._
 import common.Consts._
 
 object UartAddr {
-  val UART_TX_DATA = 0x10000000.U(WORD_LEN.W)
+  val RBR = 0x00.U(WORD_LEN.W) // Receive Buffer Register
+  val LSR = 0x05.U(WORD_LEN.W) // Line Status Register
 }
 
 class UartMemory extends Module {
@@ -13,9 +14,13 @@ class UartMemory extends Module {
     val dmem = new DmemPortIo()
   })
 
-  io.dmem.rdata := 0.U(WORD_LEN.W)
+  io.dmem.rdata := MuxLookup(io.dmem.raddr, 0.U(WORD_LEN.W))(
+    Seq(
+      UartAddr.LSR -> 0x60.U(WORD_LEN.W)
+    )
+  )
 
-  when(io.dmem.wen === MenSel.B && io.dmem.waddr === UartAddr.UART_TX_DATA) {
+  when(io.dmem.wen === MenSel.B && io.dmem.waddr === UartAddr.RBR) {
     printf("%c", io.dmem.wdata(7, 0))
   }
 }
