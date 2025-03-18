0490e5be9337a57fed350aa85b5a40208bdbec90
Arata <arata04962@gmail.com>
2025-03-18 18:00:12 +0900

    refactor: rename MmioController -> Bus

---
+++ src/main/scala/core/Bus.scala
@@ -0,0 +1,63 @@
+package core
+
+import chisel3._
+import chisel3.util._
+import chisel3.util.experimental.loadMemoryFromFileInline
+import common.Consts._
+
+object MemSel extends ChiselEnum {
+  val RAM, UART, CLINT, INVALID = Value
+}
+
+object MemRange {
+  val RAM = BitPat("b100000??_????????_????????_????????")
+  val RAM_MASK = 0x3ffffff.U(WORD_LEN.W)
+  val UART = BitPat("b00010000_00000000_00000000_????????")
+  val UART_MASK = 0xff.U(WORD_LEN.W)
+}
+
+class Bus extends Module {
+  val io = IO(new Bundle {
+    val dmem_in = new DmemPortIo()
+    val dmem_out_memory = Flipped(new DmemPortIo())
+    val dmem_out_uart = Flipped(new DmemPortIo())
+  })
+
+  def addr_to_sel(addr: UInt): MemSel.Type = {
+    MuxCase(
+      MemSel.INVALID,
+      Seq(
+        (addr === MemRange.RAM) -> MemSel.RAM,
+        (addr === MemRange.UART) -> MemSel.UART,
+      )
+    )
+  }
+
+  val rsel = addr_to_sel(io.dmem_in.raddr)
+  val wsel = addr_to_sel(io.dmem_in.waddr)
+
+  when(io.dmem_in.wen =/= MenSel.X && wsel === MemSel.INVALID) {
+    printf(
+      "[Bus] Invalid memory write at 0x%x = 0x%x\n",
+      io.dmem_in.waddr,
+      io.dmem_in.wdata
+    )
+  }
+
+  io.dmem_out_memory.raddr := io.dmem_in.raddr & MemRange.RAM_MASK
+  io.dmem_out_memory.wen := Mux(wsel === MemSel.RAM, io.dmem_in.wen, MenSel.X)
+  io.dmem_out_memory.waddr := io.dmem_in.waddr & MemRange.RAM_MASK
+  io.dmem_out_memory.wdata := io.dmem_in.wdata
+
+  io.dmem_out_uart.raddr := io.dmem_in.raddr & MemRange.UART_MASK
+  io.dmem_out_uart.wen := Mux(wsel === MemSel.UART, io.dmem_in.wen, MenSel.X)
+  io.dmem_out_uart.waddr := io.dmem_in.waddr & MemRange.UART_MASK
+  io.dmem_out_uart.wdata := io.dmem_in.wdata
+
+  io.dmem_in.rdata := MuxLookup(rsel, 0.U(WORD_LEN.W))(
+    Seq(
+      MemSel.RAM -> io.dmem_out_memory.rdata,
+      MemSel.UART -> io.dmem_out_uart.rdata,
+    )
+  )
+}
--- src/main/scala/core/Top.scala
+++ src/main/scala/core/Top.scala
@@ -16,16 +16,16 @@ class Top(memoryFile: String, suppressLog: Boolean) extends Module {
   val regfile = Module(new RegFile())
   val csrfile = Module(new CsrFile())
   val core = Module(new Core(suppressLog))
-  val mmio = Module(new MmioController())
+  val bus = Module(new Bus())
   val memory = Module(new Memory(memoryFile))
   val uart_memory = Module(new UartMemory())
 
   core.io.imem <> memory.io.imem
-  core.io.dmem <> mmio.io.dmem_in
+  core.io.dmem <> bus.io.dmem_in
   core.io.regfile <> regfile.io
   core.io.csrfile <> csrfile.io
-  mmio.io.dmem_out_memory <> memory.io.dmem
-  mmio.io.dmem_out_uart <> uart_memory.io.dmem
+  bus.io.dmem_out_memory <> memory.io.dmem
+  bus.io.dmem_out_uart <> uart_memory.io.dmem
   io.gp := core.io.gp
   io.pc := core.io.pc
   io.inst := core.io.inst
